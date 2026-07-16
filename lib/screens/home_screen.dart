dd20a5613e7c5aed0255c1947549c8d8d99e9c00
Pratik Kumar <itachi.op.013@gmail.com>
2026-07-17 03:48:38 +0530

    updating drills screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:soloshuttle/screens/drills_screen.dart';
-import 'drills_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
